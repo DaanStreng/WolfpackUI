bd39bc84e6b004577c6173fb0cc7fa5092b34947
Daan Streng <daanstreng@gmail.com>
2018-03-06 14:12:14 +0100

    made nested properties available for use

--- data/datalock.js
+++ data/datalock.js
@@ -1,46 +1,32 @@
 Element.prototype.datalockObjectChanged = function(property, oldval, newval) {
+    console.log("because change");
     var datalock = $(this).data("datalock");
+    console.log(this, datalock);
     this.wpuiFill(datalock);
     return newval;
     var d = 1;
 }
-Element.prototype.wpuiFill = function(baseObject) {
+Element.prototype.wpuiFill = function(baseObject, parent) {
+   
     var me = $(this);
     var children = me.children();
+    //Check what the original contennt, without replacements was.
+    //This will allow us to keep update from the original template
+    var originalContent = $(this).data("wpuiOriginalFill");
+    var htmlContent = false;
+    //Make sure the original content is set properly
+    if (!originalContent) {
+        originalContent = $(this).html();
+        $(this).data("wpuiOriginalFill", originalContent);
+    }
     if (children.length == 0) {
-        var originalContent = $(this).data("wpuiOriginalFill");
-        if (!originalContent) {
-            originalContent = $(this).html();
-            $(this).data("wpuiOriginalFill", originalContent);
-        }
-        var htmlContent = originalContent;
-        if (htmlContent.indexOf("{{") != -1) {
-
-            var splits = htmlContent.split("{{");
-            var items = [];
-            for (var i = 0; i < splits.length; i++) {
-                if (splits[i].indexOf("}}") != -1) {
-                    var subSplit = splits[i].substr(0, splits[i].indexOf("}}"));
-                    items.push(subSplit);
-                }
-            }
-            for (var i = 0; i < items.length; i++) {
-                var key = items[i];
-                var value;
-                if (typeof baseObject == "object") {
-                    if (baseObject[key]) {
-                        value = baseObject[key];
-                    }
-                }
-                else if (key === "value") {
-                    value = baseObject;
-                }
-                htmlContent = htmlContent.replace("{{" + key + "}}", value);
-            }
-            $(this).html(htmlContent);
-        }
+        //If there are no child objects we should fill the object from the
+        //original template
+        htmlContent = originalContent;
+        
     }
     else {
+        //Recursively fill out all the children of the template
         children.each(function() {
             var child = $(this);
 
@@ -60,11 +46,94 @@ Element.prototype.wpuiFill = function(baseObject) {
                 this.wpuiIf(baseObject, child.attr("wpui-if"));
             }
             else if (!$(this).attr("wpui-stop") && !$(this).data("datalock")) {
-                this.wpuiFill(baseObject);
+                if(parent!==undefined)
+                    this.wpuiFill(baseObject,parent);
+                else
+                    this.wpuiFill(baseObject,me[0]);
             }
         });
+        htmlContent = $(this).html();
     }
 
+    //The magical part
+    //After filling all the children according to the template we fill the
+    //Whole template with the base object. The result being that if any
+    //children use the values from the baseobject within their templates
+    //by useing baseobject.property these will get filled as expected as well
+    if (htmlContent.indexOf("{{") != -1) {
+
+        var splits = htmlContent.split("{{");
+        var items = [];
+        for (var i = 0; i < splits.length; i++) {
+            if (splits[i].indexOf("}}") != -1) {
+                var subSplit = splits[i].substr(0, splits[i].indexOf("}}"));
+                items.push(subSplit);
+            }
+        }
+        for (var i = 0; i < items.length; i++) {
+            var key = items[i];
+            var value;
+            if (typeof baseObject == "object") {
+                if (baseObject[key]) {
+                    value = baseObject[key];
+                }
+                else {
+                    var getter = "value = baseObject." + key;
+                    eval(getter);
+                    if (value === undefined) {
+                        getter = "value = baseObject" + key;
+                        eval(getter);
+                    }
+                    console.log(getter,value);
+                    var pointIndex = key.lastIndexOf(".");
+                    var bracketIndex = key.lastIndexOf("[");
+                    var locatedObject = "";
+                    var locatedProperty = "";
+                    var sparent = this;
+                    if(parent !==undefined){
+                        console.log(parent);
+                        sparent = parent;
+                    }else console.log(this);
+                    if(pointIndex>bracketIndex){
+                      
+                        locatedObject = key.substr(0,pointIndex);
+                        locatedProperty = key.substr(pointIndex+1);
+                        var watchVars = $(sparent).data("watched");
+                        if(!watchVars){
+                            watchVars = [];
+                        }
+                        if (watchVars.indexOf(key)==-1){
+                            watchVars.push(key);
+                            $(sparent).data("watched",watchVars);
+                            console.log(sparent);
+                            var datalockEval = "baseObject."+locatedObject+".watch(\""+locatedProperty+"\", sparent, sparent.datalockObjectChanged);";
+                            console.log(datalockEval);
+                            eval(datalockEval);
+                        }
+                    }
+                    else if (pointIndex<bracketIndex){
+                        locatedObject = key.substr(0,bracketIndex);
+                        locatedProperty = key.substr(bracketIndex+1);
+                        if(locatedProperty.indexOf("'")>0||locatedProperty.indexOf('"')>0){
+                            locatedProperty = locatedProperty.replace("'","").replace('"',"");
+                            var datalockEval = "baseObject."+locatedObject+".watch(\""+locatedProperty+"\", sparent, sparent.datalockObjectChanged);";
+                            eval(datalockEval);
+                        }
+                    }
+                   
+                }
+            }
+            else if (key === "value") {
+                value = baseObject;
+            }
+            if(value!=='undefined')
+                htmlContent = htmlContent.replace("{{" + key + "}}", value);
+        }
+        $(this).html(htmlContent);
+    }
+
+
+    //Setting the attributes of the ellement to allwo templating
     for (var i = 0, atts = this.attributes, n = atts.length, arr = []; i < n; i++) {
         arr.push(atts[i].nodeName);
 
@@ -220,6 +289,7 @@ jQuery.fn.datalock = function(dataobject) {
         }
     }
     for (var i = 0; i < this.length; i++) {
+        console.log("doing this one",this[i]); 
         this[i].wpuiFill(dataobject);
     }
 

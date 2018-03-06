77f0d1abec4b4ca9281219c9d5d1c0eb0bbfe801
Daan Streng <daanstreng@gmail.com>
2018-03-06 12:28:34 +0100

    Made sure that locking also works on an object that is templated itself. So now if you set a WPUI-attribute on an element you can also datalock that element

--- data/datalock.js
+++ data/datalock.js
@@ -1,192 +1,252 @@
-Element.prototype.datalockObjectChanged = function(property,oldval,newval){
+Element.prototype.datalockObjectChanged = function(property, oldval, newval) {
     var datalock = $(this).data("datalock");
-    
     this.wpuiFill(datalock);
     return newval;
-    
 }
-Element.prototype.wpuiFill = function(baseObject){
-    
+Element.prototype.wpuiFill = function(baseObject) {
     var me = $(this);
     var children = me.children();
-    if(children.length == 0){
-        
+    if (children.length == 0) {
         var originalContent = $(this).data("wpuiOriginalFill");
-        if(!originalContent){
+        if (!originalContent) {
             originalContent = $(this).html();
-            $(this).data("wpuiOriginalFill",originalContent);
+            $(this).data("wpuiOriginalFill", originalContent);
         }
-        
         var htmlContent = originalContent;
-        if(htmlContent.indexOf("{{")!=-1){
-            
+        if (htmlContent.indexOf("{{") != -1) {
+
             var splits = htmlContent.split("{{");
             var items = [];
-            for(var i = 0;i<splits.length;i++){
-                if(splits[i].indexOf("}}")!=-1){
-                    var subSplit = splits[i].substr(0,splits[i].indexOf("}}"));
+            for (var i = 0; i < splits.length; i++) {
+                if (splits[i].indexOf("}}") != -1) {
+                    var subSplit = splits[i].substr(0, splits[i].indexOf("}}"));
                     items.push(subSplit);
                 }
             }
-            for(var i = 0;i<items.length;i++){
+            for (var i = 0; i < items.length; i++) {
                 var key = items[i];
                 var value;
-                if (typeof baseObject == "object"){
-                    if(baseObject[key]){
+                if (typeof baseObject == "object") {
+                    if (baseObject[key]) {
                         value = baseObject[key];
                     }
-                }else if (key === "value"){
+                }
+                else if (key === "value") {
                     value = baseObject;
                 }
-                htmlContent = htmlContent.replace("{{"+key+"}}",value);
+                htmlContent = htmlContent.replace("{{" + key + "}}", value);
             }
-            
             $(this).html(htmlContent);
         }
-    }else{
-        children.each(function(){
+    }
+    else {
+        children.each(function() {
             var child = $(this);
-            
-            if(child.attr("wpui-for")){
+
+            if (child.attr("wpui-for")) {
                 var forKey = child.attr("wpui-for");
-                if(forKey && baseObject[forKey] && baseObject[forKey].constructor === Array){
+                if (forKey && baseObject[forKey] && baseObject[forKey].constructor === Array) {
                     this.wpuiFor(baseObject[forKey]);
                 }
             }
-            else if(child.attr("wpui-object")){
+            else if (child.attr("wpui-object")) {
                 var forKey = child.attr("wpui-object");
-                if(forKey && baseObject[forKey] && typeof baseObject[forKey] == "object"){
-                    child.datalock(baseObject[forKey]); 
+                if (forKey && baseObject[forKey] && typeof baseObject[forKey] == "object") {
+                    child.datalock(baseObject[forKey]);
                 }
             }
-            else if (child.attr("wpui-if")){
-                this.wpuiIf(baseObject,child.attr("wpui-if"));
+            else if (child.attr("wpui-if")) {
+                this.wpuiIf(baseObject, child.attr("wpui-if"));
             }
-            else if(!$(this).attr("wpui-stop")&&!$(this).data("datalock")){
+            else if (!$(this).attr("wpui-stop") && !$(this).data("datalock")) {
                 this.wpuiFill(baseObject);
             }
         });
     }
-}
-Element.prototype.wpuiIf = function(dataObject,key){
-    var value = checkValidity(key,dataObject);;
-    var isfalseString = (value === 'false');
-    
-    var ifContent = $(this).data("wpuiIfContent");
-    if(!ifContent){
-        ifContent = $(this).html();
-        $(this).data("wpuiIfContent",ifContent);
+
+    for (var i = 0, atts = this.attributes, n = atts.length, arr = []; i < n; i++) {
+        arr.push(atts[i].nodeName);
+
+        var atr = $(this).attr(atts[i].nodeName);
+
+        if (atr.indexOf("{{") != -1) {
+            var splits = atr.split("{{");
+            var items = [];
+            for (var i = 0; i < splits.length; i++) {
+                if (splits[i].indexOf("}}") != -1) {
+                    var subSplit = splits[i].substr(0, splits[i].indexOf("}}"));
+                    items.push(subSplit);
+                }
+            }
+            for (var i = 0; i < items.length; i++) {
+                var key = items[i];
+                var value;
+                if (typeof baseObject == "object") {
+                    if (baseObject[key]) {
+                        value = baseObject[key];
+                    }
+                }
+                else if (key === "value") {
+                    value = baseObject;
+                }
+                atr = atr.replace("{{" + key + "}}", value);
+            }
+            $(this).attr(atts[i].nodeName, atr);
+        }
+
     }
-    
-    if(!value||isfalseString){
+
+}
+Element.prototype.wpuiIf = function(dataObject, key) {
+    var value = checkValidity(key, dataObject);;
+    var isfalseString = (value === 'false');
+
+    var ifContent = $(this).data("wpuiIfContent");
+    if (!ifContent) {
+        ifContent = $(this).html();
+        $(this).data("wpuiIfContent", ifContent);
+    }
+
+    if (!value || isfalseString) {
         $(this).empty();
         var siblings = $(this).nextAll();
         var foundTrue = false;
-        siblings.each(function(){
-            if ($(this).attr("wpui-if")){
+        siblings.each(function() {
+            if ($(this).attr("wpui-if")) {
                 //will get filled another way
                 foundTrue = true;
                 return;
             }
-            if($(this).attr("wpui-elseif")){
+            if ($(this).attr("wpui-elseif")) {
                 var key2 = $(this).attr("wpui-elseif");
-                var value2 = checkValidity(key2,dataObject);
+                var value2 = checkValidity(key2, dataObject);
                 var isfalseString = (value2 === 'false');
-                
+
                 var ifElseContent = $(this).data("wpuiIfContent");
-                if(!ifElseContent){
+                if (!ifElseContent) {
                     ifElseContent = $(this).html();
-                    $(this).data("wpuiIfContent",ifElseContent);
+                    $(this).data("wpuiIfContent", ifElseContent);
                 }
-                if(foundTrue){
+                if (foundTrue) {
                     $(this).empty();
                     return;
                 }
-                
-              
-                
-                
-                if(value2&&!isfalseString){
+
+
+
+
+                if (value2 && !isfalseString) {
                     foundTrue = true;
                     $(this).html(ifElseContent);
                     this.wpuiFill(dataObject);
-                }else{
+                }
+                else {
                     $(this).empty();
                 }
             }
-            if (typeof $(this).attr("wpui-else")!=='undefined'){
+            if (typeof $(this).attr("wpui-else") !== 'undefined') {
                 var elseContent = $(this).data("wpuiIfContent");
-                if(!elseContent){
+                if (!elseContent) {
                     elseContent = $(this).html();
-                    $(this).data("wpuiIfContent",elseContent);
+                    $(this).data("wpuiIfContent", elseContent);
                 }
-                if(!foundTrue){
+                if (!foundTrue) {
                     foundTrue = true;
                     $(this).html(elseContent);
                     this.wpuiFill(dataObject);
-                }else{
+                }
+                else {
                     $(this).empty();
                 }
             }
         });
-    }else{
+    }
+    else {
         var siblings = $(this).nextAll();
-         $(this).html(ifContent);
-        siblings.each(function(){
-            
-           if($(this).attr("wpui-elseif")||typeof $(this).attr("wpui-else")!=='undefined'){
+        $(this).html(ifContent);
+        siblings.each(function() {
+
+            if ($(this).attr("wpui-elseif") || typeof $(this).attr("wpui-else") !== 'undefined') {
                 var previousContent = $(this).data("wpuiIfContent");
-                if(!previousContent){
+                if (!previousContent) {
                     previousContent = $(this).html();
-                    $(this).data("wpuiIfContent",previousContent);
+                    $(this).data("wpuiIfContent", previousContent);
                 }
-                $(this).empty(); 
-           } 
+                $(this).empty();
+            }
         });
         this.wpuiFill(dataObject);
     }
 }
-function checkValidity(key,object){
- 
-        var tt = false;
-        key = "tt = object."+key+";";
-        eval(key);
-        return tt;
-   
+
+function checkValidity(key, object) {
+
+    var tt = false;
+    key = "tt = object." + key + ";";
+    eval(key);
+    return tt;
+
 }
 
-Element.prototype.wpuiFor = function(dataArray){
+Element.prototype.wpuiFor = function(dataArray) {
     var me = $(this);
     var originalContent = me.data("wpuiForContent");
-    if(!originalContent){
+    if (!originalContent) {
         originalContent = me.html();
-        me.data("wpuiForContent",originalContent);
+        me.data("wpuiForContent", originalContent);
     }
     me.empty();
-    for(var i =0;i<dataArray.length;i++){
+    for (var i = 0; i < dataArray.length; i++) {
         var element = $(originalContent);
         me.append(element);
         element.datalock(dataArray[i]);
     }
 }
-jQuery.fn.datalock = function(dataobject){
-    this.data("datalock",dataobject);
+jQuery.fn.datalock = function(dataobject) {
+    this.data("datalock", dataobject);
     var originalContent = this.data("wpuiOriginalContent");
-    if(!originalContent){
+    if (!originalContent) {
         originalContent = this.html();
-        this.data("wpuiOriginalContent",originalContent);
-    }else{
+        this.data("wpuiOriginalContent", originalContent);
+    }
+    else {
         this.empty();
         this.html(originalContent);
     }
-    for(var propertyName in dataobject){
-        for(var i = 0;i<this.length;i++){
-            dataobject.watch(propertyName,this[i],this[i].datalockObjectChanged);
-        }        
+    for (var propertyName in dataobject) {
+        for (var i = 0; i < this.length; i++) {
+            dataobject.watch(propertyName, this[i], this[i].datalockObjectChanged);
+        }
     }
-    for(var i = 0;i<this.length;i++){
+    for (var i = 0; i < this.length; i++) {
         this[i].wpuiFill(dataobject);
     }
-    
-}
 
+    for (var i = 0; i < this.length; i++) {
+
+        var child = $(this[i]);
+
+        if (child.attr("wpui-for")) {
+            var forKey = child.attr("wpui-for");
+            if (forKey && dataobject && dataobject.constructor === Array) {
+                this[i].wpuiFor(dataobject);
+            }
+        }
+        else if (child.attr("wpui-object")) {
+            var forKey = child.attr("wpui-object");
+            if (forKey && dataobject && typeof dataobject == "object") {
+                child.datalock(dataobject);
+            }
+        }
+        else if (child.attr("wpui-if")) {
+
+            this[i].wpuiIf(dataobject, child.attr("wpui-if"));
+        }
+        else if (!$(this[i]).attr("wpui-stop") && !$(this[i]).data("datalock")) {
+            this[i].wpuiFill(dataobject);
+        }
+        else this[i].wpuiFill(dataobject);
+
+    }
+
+}

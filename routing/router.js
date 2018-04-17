f0902104ab63d7981255a2b58478e3b8023d7274
JeroenvdV <jeroen@jeroen-vandeven.nl>
2018-04-17 21:41:24 +0200

    Auto reformat to please my IDE

--- routing/router.js
+++ routing/router.js
@@ -3,11 +3,11 @@
 import {
     Frame
 }
-from './frame.js';
+    from './frame.js';
 
 document.WPUIglobalEval = function() {
     var scripts = document.getElementsByTagName("script");
-   
+
     for (var i = 0; i < scripts.length; i++) {
         if (!scripts[i]._executed2) {
             try {
@@ -25,7 +25,7 @@ document.WPUIglobalEval = function() {
         }
     }
 }
-window.onload = function() {
+window.onload           = function() {
     var scripts = document.getElementsByTagName("script");
     for (var i = 0; i < scripts.length; i++) {
         scripts[i]._executed2 = true;
@@ -34,16 +34,20 @@ window.onload = function() {
 
 export class Router {
     constructor() {
-        if (arguments[0])
+        if (arguments[0]) {
             this.frame = arguments[0];
-        if (arguments[1])
+        }
+        if (arguments[1]) {
             this.homePage = arguments[1];
-        else
+        } else {
             this.homePage = "home";
+        }
         if (arguments[2]) {
             this.basePath = arguments[2];
         }
-        else this.basePath = "";
+        else {
+            this.basePath = "";
+        }
 
         if (arguments[3]) {
             this.defaultFileExtension = arguments[3];
@@ -51,14 +55,17 @@ export class Router {
                 this.defaultFileExtension = this.defaultFileExtension.substr(1, this.defaultFileExtension.length - 1);
             }
         }
-        else
-            this.defaultFileExtension = "js";
-        if (arguments[4])
-            this.container = arguments[4];
-        else this.container = document.body;
-        if (arguments[5])
-            this.headers = arguments[5];
         else {
+            this.defaultFileExtension = "js";
+        }
+        if (arguments[4]) {
+            this.container = arguments[4];
+        } else {
+            this.container = document.body;
+        }
+        if (arguments[5]) {
+            this.headers = arguments[5];
+        } else {
             this.headers = [
                 "<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/3.3.1\/jquery.min.js\"><\/script>",
                 "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js\"><\/script>",
@@ -66,10 +73,10 @@ export class Router {
 
             ]
         }
-        this.headersLoaded = false;
-        this.baseURL = document.location.hostname + this.basePath;
+        this.headersLoaded    = false;
+        this.baseURL          = document.location.hostname + this.basePath;
         this.container.Router = this;
-        this.catchNavigation = true;
+        this.catchNavigation  = true;
         this.loadHeaders();
         var me = this;
         window.addEventListener("popstate", function(e) {
@@ -81,6 +88,7 @@ export class Router {
     get Container() {
         return this.container;
     }
+
     getPageFromURL(requestedPath) {
 
         requestedPath = requestedPath.replace("/" + this.basePath, "");
@@ -90,7 +98,7 @@ export class Router {
             requestedPath = requestedPath.substr(1, requestedPath.length - 1);
         }
         var slashSplits = requestedPath.split("/");
-        var actualPage = slashSplits[slashSplits.length - 1];
+        var actualPage  = slashSplits[slashSplits.length - 1];
         if (actualPage.trim() == 0) {
             if (slashSplits > 1) {
                 actualPage = slashSplits[slashSplits.length - 2];
@@ -101,15 +109,18 @@ export class Router {
         }
         return actualPage;
     }
+
     SetPageFromUrl(url, noPush) {
         var actualPage = this.getPageFromURL(url);
         this.currentFrame.setPage(actualPage);
         var stateObj = {
             page: actualPage
         };
-        if (noPush == undefined || !noPush)
+        if (noPush == undefined || !noPush) {
             window.history.pushState(stateObj, actualPage, url);
+        }
     }
+
     Route() {
         if (this.headersLoaded) {
             this.directRoute();
@@ -121,32 +132,33 @@ export class Router {
             }, 1000);
         }
     }
+
     directRoute() {
         var scripts = document.getElementsByTagName("script");
         for (var i = 0; i < scripts.length; i++) {
             scripts[i]._executed = true;
         }
         var actualPage = this.getPageFromURL(document.location.pathname.replace("/" + this.basePath + "/", ""));
-        var framePath = this.basePath + "/frames/" + this.frame + "/" + this.frame + "." + this.defaultFileExtension;
-        var me = this;
+        var framePath  = this.basePath + "/frames/" + this.frame + "/" + this.frame + "." + this.defaultFileExtension;
+        var me         = this;
         import (document.location.origin + "/" + framePath).then(({
-            default: frameBase
-        }) => {
-            var frame = new frameBase(me.basePath);
+                                                                      default: frameBase
+                                                                  }) => {
+            var frame       = new frameBase(me.basePath);
             me.currentFrame = frame;
             frame.addOnPartLoadedHandlers(me, me.framePartLoaded);
             me.clearContainer();
 
             frame.getContent().then(html => {
                 //me.container.innerHTML = me.container.innerHTML + html;
-                var div = document.createElement('div');
+                var div       = document.createElement('div');
                 div.innerHTML = html.trim();
                 // Change this to div.childNodes to support multiple top-level nodes
                 for (var i = 0; i < div.childNodes.length; i++) {
                     var element = div.childNodes[i];
                     me.container.appendChild(element);
                 }
-                
+
                 document.WPUIglobalEval();
                 frame.onLoaded();
                 frame.setPage(actualPage);
@@ -163,7 +175,7 @@ export class Router {
         if (this.catchNavigation) {
             var me = this;
             for (var ls = document.links, numLinks = ls.length, i = 0; i < numLinks; i++) {
-                var href = ls[i].href;
+                var href      = ls[i].href;
                 ls[i].onclick = function() {
                     if (this.hostname != document.location.hostname) {
                         return true;
@@ -188,6 +200,7 @@ export class Router {
             this.loadHeaders();
         }
     }
+
     loadHeaders() {
         var me = this;
         window.setTimeout(function() {
@@ -199,16 +212,16 @@ export class Router {
                 var s = document.getElementsByTagName('script')[0];
                 //  document.head.innerHTML += this.headers[i].trim();
 
-                var div = document.createElement('div');
+                var div       = document.createElement('div');
                 div.innerHTML = this.headers[i].trim();
-                var element = div.firstChild;
+                var element   = div.firstChild;
                 // Change this to div.childNodes to support multiple top-level nodes
                 if (element && element.src) {
-                    var script = document.createElement('script');
+                    var script    = document.createElement('script');
                     script.onload = function() {
 
                     }
-                    script.src = element.src;
+                    script.src    = element.src;
                     document.getElementsByTagName("head")[0].appendChild(script);
                 }
                 else {
@@ -223,6 +236,7 @@ export class Router {
         }
 
     }
+
     fetchURL(url, callback) {
         fetch(url)
             .then(function(response) {
